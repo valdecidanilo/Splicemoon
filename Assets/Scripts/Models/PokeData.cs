d30f34d4f697e8302d2fe6857222b8bba417e7fd
Valdeci Danilo <valdeci.danilo@gdstecnologia.com.br>
2025-04-25 19:41:45 -0300

    fix: poke data

--- Assets/Scripts/Models/PokeData.cs
+++ Assets/Scripts/Models/PokeData.cs
@@ -23,8 +23,8 @@ namespace Models
     }
     public struct Ability
     {
-        public string name { get; set; }
-        public string url { get; set; }
+        [JsonProperty("name")] public string name { get; set; }
+        [JsonProperty("url")] public string url { get; set; }
     }
     //====
 
