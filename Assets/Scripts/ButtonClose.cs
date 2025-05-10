b4297532b46b4664cf42690011ecb94469366bdf
Valdeci Danilo <valdecidanilo@live.com>
2025-05-10 15:29:08 -0300

    feat: added icon battle in progress

---
+++ Assets/Scripts/ButtonClose.cs
@@ -0,0 +1,13 @@
+﻿using CustomButton;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClose : MonoBehaviour
+{
+    public CustomButtonClass button;
+    public Image iconSelect;
+    public Image container;
+    public Sprite[] containerSelect;
+    public void SetContainerSelected(int index) => container.sprite = containerSelect[index == 0 ? 1 : 0];
+    public void SetIconSelected(Sprite spr) => iconSelect.sprite = spr;
+}
