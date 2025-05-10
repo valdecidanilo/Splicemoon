167369f920ec59a45b5e3efc40755f69c55cfb6e
Valdeci Danilo <valdecidanilo@live.com>
2025-05-10 08:54:55 -0300

    fix: update positions and radius grass

--- Assets/Scripts/Player/BattleTriggerZone.cs
+++ Assets/Scripts/Player/BattleTriggerZone.cs
@@ -8,13 +8,15 @@ namespace Player
     {
         public LayerMask grassLayer;
         public float encounterChance = 0.1f;
+        public float originY;
+        public float radius = 0.05f;
         public BattleController battleController;
         public BattleUIManager uIManager;
         public void TryStartBattle(Action<bool> encounterBattle)
         {
-            Vector2 position = transform.position;
+            Vector2 position = new Vector2(transform.position.x, transform.position.y - originY);
 
-            var grass = Physics2D.OverlapCircle(position, 0.1f, grassLayer);
+            var grass = Physics2D.OverlapCircle(position, radius, grassLayer);
             if (grass != null)
             {
                 var roll = Random.Range(0f, 1f);
@@ -27,6 +29,13 @@ namespace Player
                     }));
                 }
             }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.cyan;
+            var origin = new Vector2(transform.position.x, transform.position.y - originY);
+            Gizmos.DrawWireSphere(origin, radius);
         }
     }
 }
