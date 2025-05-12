c395d68145dd718fdd4382eb674cfc0680ae82ed
Valdeci Danilo <valdecidanilo@live.com>
2025-05-11 21:47:41 -0300

    fix: recap playerbag

--- Assets/Scripts/PlayerBag.cs
+++ Assets/Scripts/PlayerBag.cs
@@ -9,32 +9,4 @@ public class PlayerBag : MonoBehaviour
     public SpliceMon currentSplicemon;
     public List<SpliceMon> splicemons = new ();
     public bool bagInitialized = false;
-    
-    [Header("Test")]
-    public int myPokemonTest = 1;
-    public int level = 1;
-    private void Awake()
-    {
-        StartCoroutine(ApiManager.GetPokeData(myPokemonTest, SetCurrentSplicemon));
-    }
-    private void SetCurrentSplicemon(PokeData pokeData)
-    {
-
-        var child = new GameObject(pokeData.NameSpliceMoon);
-        child.transform.SetParent(gameObject.transform);
-        currentSplicemon = child.AddComponent<SpliceMon>();
-        currentSplicemon.level = level;
-        currentSplicemon.Initialize(pokeData);
-        bagInitialized = true;
-        var fourfirst = currentSplicemon.stats.possiblesMoveAttack.Take(4).ToList();
-        StartCoroutine(ApiManager.GetMoveDetails(fourfirst, moveAttackList =>
-        {
-            
-            for (var i = 0; i < 4; i++)
-            {
-                moveAttackList[i].ppCurrent = moveAttackList[i].ppMax;
-            }
-            currentSplicemon.stats.movesAttack = new List<MoveDetails>(moveAttackList);
-        }));
-    }
 }
