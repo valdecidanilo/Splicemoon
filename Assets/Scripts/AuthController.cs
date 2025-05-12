72f6af30f3286d16dde18329e09ed60e4a803638
Valdeci Danilo <valdecidanilo@live.com>
2025-05-11 21:45:28 -0300

    feat: added auth controller

---
+++ Assets/Scripts/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using DB;
+using DB.Data;
+using UnityEngine;
+public class AuthController : MonoBehaviour
+{
+    public Database Database;
+    
+    public UserData Login(string email, string password)
+    {
+        return Database.LoginUser(email, password);
+    }
+    public (bool,string, int) Register(string email, string password, string nickname)
+    {
+        return Database.RegisterUser(email, password, nickname);
+    }
+}
