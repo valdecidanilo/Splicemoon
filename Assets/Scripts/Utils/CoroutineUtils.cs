805e8deb912f0b320db40811712eeddce70c7a2f
Valdeci Danilo <valdecidanilo@live.com>
2025-05-12 21:12:16 -0300

    feat: coroutines uitls

---
+++ Assets/Scripts/Utils/CoroutineUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public static class CoroutineUtils
+    {
+        public static IEnumerator AsIEnumerator<T>(this Task<T> task, Action<T> callback)
+        {
+            while (!task.IsCompleted)
+                yield return null;
+
+            if (task.IsFaulted && task.Exception != null)
+                throw task.Exception;
+
+            callback(task.Result);
+        }
+    }
+}
