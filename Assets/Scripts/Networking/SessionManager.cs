e1ff1160cb5e28d5d4085bf798a74e5ae2c49c1b
Valdeci Danilo <valdecidanilo@live.com>
2025-05-12 21:11:49 -0300

    feat: added session script manager

---
+++ Assets/Scripts/Networking/SessionManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using DB.Data;
+using UnityEngine;
+
+namespace Networking
+{
+    public class SessionManager : MonoBehaviour
+    {
+        public static SessionManager Instance { get; private set; }
+    
+        public UserData CurrentUser { get; private set; }
+        public List<SplicemonData> SplicemonDataList { get; private set; }
+        public string Nickname { get; private set; }
+    
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    
+        public void SetSessionData(UserData userData, List<SplicemonData> splicemonDataList, string nickname)
+        {
+            CurrentUser = userData;
+            SplicemonDataList = splicemonDataList;
+            Nickname = nickname;
+        }
+    }
+}
